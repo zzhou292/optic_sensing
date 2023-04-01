4e7bac989c2acc5e17b6daed1feb55db3c13c0ae
zzhou292 <zzhou292@wisc.edu>
2023-04-01 01:36:16 -0500

    Add logging

--- Phidget22Python/test.py
+++ Phidget22Python/test.py
@@ -1,11 +1,31 @@
 from Phidget22.Phidget import *
 from Phidget22.Devices.Accelerometer import *
 import time
+import csv
+
+# The CSV file path
+csv_file_path = 'my_file.csv'
 
 def onAccelerationChange(self, acceleration, timestamp):
+
+	
 	print("Acceleration: \t"+ str(acceleration[0])+ "  |  "+ str(acceleration[1])+ "  |  "+ str(acceleration[2]))
 	print("Timestamp: " + str(timestamp))
 	print("----------")
+
+	# Open the CSV file for writing
+	with open(csv_file_path, mode='a', newline='') as csv_file:
+		# Create a CSV writer object
+		csv_writer = csv.writer(csv_file, delimiter=',')
+
+		row_data =[]
+		row_data.append(timestamp)
+		row_data.append(acceleration[0])
+		row_data.append(acceleration[1])
+		row_data.append(acceleration[2])
+
+		# Write the float array to the CSV file
+		csv_writer.writerow(row_data)
 
 def main():
 	accelerometer0 = Accelerometer()
@@ -18,7 +38,6 @@ def main():
 		input("Press Enter to Stop\n")
 	except (Exception, KeyboardInterrupt):
 		pass
-
 	accelerometer0.close()
 
 main()
